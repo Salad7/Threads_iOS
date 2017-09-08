be1607794f1051dce8c8c9dc005f56fc027e7b59
mohamed <msalad@uncc.edu>
2017-09-08 04:51:31 -0400

    added messages

--- Threads/Post.swift
+++ Threads/Post.swift
@@ -10,5 +10,68 @@ import Foundation
 
 
 class Post {
+    var ucode = ""
+    var upvotes = 0
+    var messages = [Message]()
+    var replies = 0
+    var timeStamp = Date()
+    var postTitle = ""
+    var parent = ""
+    
+    
+    init(){
+        
+    }
+    
+    func setUcode(u :String){
+        ucode = u
+    }
+    func setUpvotes(up :Int){
+        upvotes = up
+    }
+    func setMessages(m :[Message]){
+        messages = m
+    }
+    func setReplies(r :Int){
+        replies = r
+    }
+    func setTimeStamp(t :Date){
+        timeStamp = t
+    }
+    func setPostTitle(p :String){
+        postTitle = p
+    }
+    func setParent(pa :String){
+        parent = pa
+    }
+    
+    func getUcode() -> String {
+        return ucode
+    }
+    
+    func getUpvotes() -> Int {
+        return upvotes
+    }
+    
+    func getMessages() -> [Message] {
+        return messages
+    }
+    
+    func getReplies() -> Int {
+        return replies
+    }
+    
+    func getTimeStamp() -> Date {
+        return timeStamp
+    }
+    
+    func getPostTitle() -> String{
+        return postTitle
+    }
+    
+    func getParent() -> String {
+        return parent
+    }
+    
     
 }
